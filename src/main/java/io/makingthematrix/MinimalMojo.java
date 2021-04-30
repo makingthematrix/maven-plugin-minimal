9c11b2dddbd01f894fd7eb27c144576659af5950
Maciej Gorywoda <makingthematrix@gmail.com>
2021-04-30 18:42:05 +0200

    initial commit

---
+++ src/main/java/io/makingthematrix/MinimalMojo.java
@@ -0,0 +1,38 @@
+package io.makingthematrix;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+record KaBoom(String foo) {}
+
+@Mojo(name = "minimal", defaultPhase = LifecyclePhase.INITIALIZE)
+public class MinimalMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        final var kaboom = new KaBoom("foo");
+        getLog().info("Hello, world! " + kaboom);
+    }
+}
